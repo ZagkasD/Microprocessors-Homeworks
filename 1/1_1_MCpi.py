ab2d5301071c3cb823d04b5ffafb55f48911fea9
ZagkasD <72259822+ZagkasD@users.noreply.github.com>
2022-11-08 01:12:58 +0200

    1.1 Done

--- 1/1_1_MCpi.py
+++ 1/1_1_MCpi.py
@@ -1,27 +1,38 @@
 # Monte Carlo algorithm for calculating pi
 
-# May the gods forgive me for what I'm about to create
+# May the gods forgive me for the code I'm about to write
+
+from math import sqrt
+import random
 
 # Counts the number of points inside the circle
-def check_point_position(points_matrix):
+def check_point_position(sample_size, points_matrix):
     counter = 0
-    #TODO check distance of each point from the center of the circle
+    for i in range(sample_size):
+        # Calculate distance of point from center of circle (0.5, 0.5)
+        dist = sqrt((points_matrix[i][0]-0.5)**2 + (points_matrix[i][1]-0.5)**2)
+        
+        if dist < 0.5:
+            counter += 1
+
     return counter
 
-# Generates the number of (x,y) points and stores them into a matrix
-def gen_points(num_of_points):
-    points
-    #TODO Check for random number generators
-    return points_matrix
 
 def main():
     a = 1   # The length of each side of the rectangular
-    points_matrix = []
-    num_of_points = 10
-    points_matrix = gen_points(num_of_points)
-    counter = check_point_position()
+    counter = 0 # Number of points inside the circle with radius = 0.5
+
+    sample_size = 10000 # Number of points
+    print('Sample size = ' + str(sample_size))
     
+    points_matrix = [[round(random.random(),2) for e in range(2)] for e in range(sample_size)]
+    #print(points_matrix)
+    counter = check_point_position(sample_size, points_matrix) 
+    print('Points inside the circle = '+str(counter))
+    points_ratio = counter/(sample_size) # Points inside the circle / all points
+    pi_approx = points_ratio * 4
+    print('Π = '+str(pi_approx))
+   
+       
 
 main()
-
-# This is test commit 1
