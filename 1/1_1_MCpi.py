e04016a7b6dadc939ae1b28731a542c74e4b6fd9
ZagkasD <72259822+ZagkasD@users.noreply.github.com>
2022-11-07 23:14:43 +0200

    Add files via upload

---
+++ 1/1_1_MCpi.py
@@ -0,0 +1,25 @@
+# Monte Carlo algorithm for calculating pi
+
+# May the gods forgive me for what I'm about to create
+
+# Counts the number of points inside the circle
+def check_point_position(points_matrix):
+    counter = 0
+    #TODO check distance of each point from the center of the circle
+    return counter
+
+# Generates the number of (x,y) points and stores them into a matrix
+def gen_points(num_of_points):
+    points
+    #TODO Check for random number generators
+    return points_matrix
+
+def main():
+    a = 1   # The length of each side of the rectangular
+    points_matrix = []
+    num_of_points = 10
+    points_matrix = gen_points(num_of_points)
+    counter = check_point_position()
+    
+
+main()
